1c8a5bf883454a16fb2ecc517345ea540764e156
wandd1n <wandersonsosa489@gmail.com>
2023-04-05 19:41:41 -0300

    js

--- Projeto - The Last Of Us/src/js/index.js
+++ Projeto - The Last Of Us/src/js/index.js
@@ -9,37 +9,40 @@ OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de fundo corres
 - passo 6 - fazer aparecer a imagem  de fundo correspondente ao botão clicado;
 */
 
-const botoesCarrossel = document.querySelectorAll('.botao');
+const botoesCarrossel = document.querySelectorAll('.botao'); /* Construir uma lista com todas as classes "botao" */
 const imagens = document.querySelectorAll('.imagem');
 
-botoesCarrossel.forEach((botao, indice) => {
-    botao.addEventListener('click', () => {
+botoesCarrossel.forEach((botao, indice) => { /* Percorrer cada elemento daquela lista ..*/
+    botao.addEventListener('click', () => { /* e adiciona um evento de clique a eles, executando com os dois parâmetros.
+    
+    () => { } indica que a expressão à esquerda da seta é o parâmetro da função e a expressão à direita é o corpo da função
+    */
         
-        desativarBotaoSelecionado();
+        desativarBotaoSelecionado(); /* remove a classe "selecionado" do botao*/
 
-        selecionarBotaoCarrossel(botao); 
+        selecionarBotaoCarrossel(botao); /* diciona a classe "selecionado" ao botão que foi clicado. */
 
-        esconderImagemAtiva();
+        esconderImagemAtiva(); /* emove a classe "ativa" da imagem que estava sendo exibida */
 
-        mostrarImagemDeFundo(indice);
+        mostrarImagemDeFundo(indice); /* adiciona a classe "ativa" à imagem que deve ser exibida com base no índice do botão clicado. */
     })
 })
 
 function mostrarImagemDeFundo(indice) {
-    imagens[indice].classList.add('ativa');
+    imagens[indice].classList.add('ativa'); /* var imagens recebe um indice, para quando houver o clique, esta img que receba a class "ativa", conquequentemente a que irá mostrar a imagem dela de fundo */
 }
 
 function selecionarBotaoCarrossel(botao) {
-    botao.classList.add('selecionado');
+    botao.classList.add('selecionado'); /* Adiciona a classe "selecinado" àquele botão clicado */
 }
 
 function esconderImagemAtiva() {
     const imagemAtiva = document.querySelector('.ativa');
-    imagemAtiva.classList.remove('ativa');
+    imagemAtiva.classList.remove('ativa'); /* Busca a imagem com a classe "ativa". Após isso, remove essa classe desta imagem */
 }
 
 function desativarBotaoSelecionado() {
     const botaoSelecionado = document.querySelector('.selecionado');
-    botaoSelecionado.classlist.remove('selecionado');
+    botaoSelecionado.classlist.remove('selecionado'); /* Busca o botão com a classe selecionado. Após isso, remove essa classe deste botão */
 }
 
